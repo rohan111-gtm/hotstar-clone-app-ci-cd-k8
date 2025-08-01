bd44e0b6c035f7622bbfb44dd1badf6a7b866cfa
rohan111-gtm <rohanrajmane555@gmail.com>
2025-08-01 21:39:19 +0530

    Update backend.tf
    bucket name updated

--- EKS_TERRAFORM/backend.tf
+++ EKS_TERRAFORM/backend.tf
@@ -1,6 +1,6 @@
 terraform {
   backend "s3" {
-    bucket = "akhotstarbucket-akas" # Replace with your actual S3 bucket name
+    bucket = "akhotstarbucket-rohan" # Replace with your actual S3 bucket name
     key    = "EKS/terraform.tfstate"
     region = "us-east-1"
   }
